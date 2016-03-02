1ce2a2bb34f0da0832fb5b2042a0063db4ab1c16
Olivia Wilson <olivia@rtwilson.com>
2016-03-02 13:23:34 +0000

    Added comments for steps

--- medial.py
+++ medial.py
@@ -12,3 +12,40 @@ Steps involved:
 import numpy as np
 import matplotlib as plt
 from scipy.ndimage.filters import laplace
+from skimage.data import coins
+
+def medial(image, visualise=False):
+    """Creates a medial axis transform image
+    
+    Args
+    ----
+    image: ndarray
+        The image of which the medial axis is to be calculated.
+        This should be a binary image.
+    visualise: bool
+        Option to visualise the medial axis transform.
+
+    Returns
+    -------
+    out: ndarray
+        A boolean image with the medial axis pixels
+    """
+
+    # Find border pixel locations of boolean image
+
+    # Create empty distance image
+    dist = np.zeros(image.shape)
+
+    # Label all border pixels to have a distance of zero in distance image
+
+    # Calculate distance to all border pixels for each non-border pixel
+
+    # 
+
+def coins():
+    """Thresholds and removes noise of coins image
+
+    Returns
+    -------
+    out: ndarray (bool)
+    """
